626975efdc31d9c9aab7ac55b6c2c68f632bd8ad
hassan <hassansultann447@gmail.com>
2026-04-17 03:11:30 +0500

    pr

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 
 
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', views.create_post, name='create_post'),
 ]
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 
 
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import PostForm
 
 def home(request):
     posts = Post.objects.all()
@@ -9,3 +12,14 @@ def home(request):
         'posts': posts,
     }
     return render(request, 'blog/home.html', context)
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'blog/create_post.html', {'form': form})
